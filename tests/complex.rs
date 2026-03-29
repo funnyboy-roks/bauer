994fdf1c3571fb7be2ef7ce990e9c0cf936d838e
funnyboy-roks <fbr@fbr.dev>
2026-03-29 03:28:49 -0500

    clenaup simple/complex tests

--- tests/complex.rs
+++ tests/complex.rs
@@ -1,53 +1,57 @@
 #![allow(dead_code)]
 
+// enusre that the same functionality is generated for all kinds (complex)
+
 use bauer::Builder;
 
-#[test]
-fn complex_owned() {
-    #[derive(Debug, Builder, PartialEq)]
-    #[builder(kind = "owned")]
-    pub struct Foo {
-        pub field_a: u32,
-        #[builder(default)]
-        pub field_b: u32,
-        #[builder(default = "42")]
-        pub field_c: u32,
-        #[builder(default, into)]
-        pub field_d: String,
-        #[builder(default = "\"hello\"", into)]
-        pub field_e: String,
-        #[builder(into)]
-        pub field_f: String,
-        #[builder(repeat)]
-        pub field_g: Vec<u32>,
-        #[builder(repeat, rename = "field_h_single")]
-        pub field_h: Vec<u32>,
-        #[builder(repeat, repeat_n = 1..=3)]
-        pub field_i: Vec<u32>,
-        #[builder(repeat = char)]
-        pub field_j: String,
-    }
+macro_rules! define {
+    ($name: ident, $kind: literal) => {
+        #[derive(Debug, Builder, PartialEq)]
+        #[builder(kind = $kind)]
+        pub struct $name {
+            pub field_a: u32,
+            #[builder(default)]
+            pub field_b: u32,
+            #[builder(default = "42")]
+            pub field_c: u32,
+            #[builder(default, into)]
+            pub field_d: String,
+            #[builder(default = "\"hello\"", into)]
+            pub field_e: String,
+            #[builder(into)]
+            pub field_f: String,
+            #[builder(repeat)]
+            pub field_g: Vec<u32>,
+            #[builder(repeat, rename = "field_h_single")]
+            pub field_h: Vec<u32>,
+            #[builder(repeat, repeat_n = 1..=3)]
+            pub field_i: Vec<u32>,
+            #[builder(repeat = char)]
+            pub field_j: String,
+        }
+    };
+}
 
-    let x = Foo::builder()
-        .field_a(5)
-        .field_f("world")
-        .field_g(0)
-        .field_g(1)
-        .field_h_single(2)
-        .field_h_single(3)
-        .field_i(4)
-        .field_i(5)
-        .field_i(6)
-        .field_j('h')
-        .field_j('i')
-        .build()
-        .unwrap();
+macro_rules! populate {
+    ($name: ident) => {
+        $name::builder()
+            .field_a(5)
+            .field_f("world")
+            .field_g(0)
+            .field_g(1)
+            .field_h_single(2)
+            .field_h_single(3)
+            .field_i(4)
+            .field_i(5)
+            .field_i(6)
+            .field_j('h')
+            .field_j('i')
+    };
+}
 
-    dbg!(&x);
-
-    assert_eq!(
-        x,
-        Foo {
+macro_rules! expected {
+    ($name: ident) => {
+        $name {
             field_a: 5,
             field_b: 0,
             field_c: 42,
@@ -59,124 +63,29 @@ fn complex_owned() {
             field_i: vec![4, 5, 6],
             field_j: String::from("hi"),
         }
-    );
+    };
+}
+
+#[test]
+fn complex_owned() {
+    define!(Foo, "owned");
+    let x = populate!(Foo).build().unwrap();
+    dbg!(&x);
+    assert_eq!(x, expected!(Foo));
 }
 
 #[test]
 fn complex_borrowed() {
-    #[derive(Debug, Builder, PartialEq)]
-    #[builder(kind = "borrowed")]
-    pub struct Foo {
-        pub field_a: u32,
-        #[builder(default)]
-        pub field_b: u32,
-        #[builder(default = "42")]
-        pub field_c: u32,
-        #[builder(default, into)]
-        pub field_d: String,
-        #[builder(default = "\"hello\"", into)]
-        pub field_e: String,
-        #[builder(into)]
-        pub field_f: String,
-        #[builder(repeat)]
-        pub field_g: Vec<u32>,
-        #[builder(repeat, rename = "field_h_single")]
-        pub field_h: Vec<u32>,
-        #[builder(repeat, repeat_n = 1..=3)]
-        pub field_i: Vec<u32>,
-        #[builder(repeat = char)]
-        pub field_j: String,
-    }
-
-    let x = Foo::builder()
-        .field_a(5)
-        .field_f("world")
-        .field_g(0)
-        .field_g(1)
-        .field_h_single(2)
-        .field_h_single(3)
-        .field_i(4)
-        .field_i(5)
-        .field_i(6)
-        .field_j('h')
-        .field_j('i')
-        .build()
-        .unwrap();
-
+    define!(Foo, "borrowed");
+    let x = populate!(Foo).build().unwrap();
     dbg!(&x);
-
-    assert_eq!(
-        x,
-        Foo {
-            field_a: 5,
-            field_b: 0,
-            field_c: 42,
-            field_d: String::from(""),
-            field_e: String::from("hello"),
-            field_f: String::from("world"),
-            field_g: vec![0, 1],
-            field_h: vec![2, 3],
-            field_i: vec![4, 5, 6],
-            field_j: String::from("hi"),
-        }
-    );
+    assert_eq!(x, expected!(Foo));
 }
 
 #[test]
 fn complex_type_state() {
-    #[derive(Debug, Builder, PartialEq)]
-    #[builder(kind = "type-state")]
-    pub struct Foo {
-        field_a: u32,
-        #[builder(default)]
-        field_b: u32,
-        #[builder(default = "42")]
-        field_c: u32,
-        #[builder(default, into)]
-        field_d: String,
-        #[builder(default = "\"hello\"", into)]
-        field_e: String,
-        #[builder(into)]
-        field_f: String,
-        #[builder(repeat)]
-        field_g: Vec<u32>,
-        #[builder(repeat, rename = "field_h_single")]
-        field_h: Vec<u32>,
-        #[builder(repeat, repeat_n = 1..=3)]
-        field_i: Vec<u32>,
-        #[builder(repeat = char)]
-        field_j: String,
-    }
-
-    let x = Foo::builder()
-        .field_a(5)
-        .field_f("world")
-        .field_g(0)
-        .field_g(1)
-        .field_h_single(2)
-        .field_h_single(3)
-        .field_i(4)
-        .field_i(5)
-        .field_i(6)
-        .field_j('h')
-        .field_j('i')
-        .build();
-
+    define!(Foo, "type-state");
+    let x = populate!(Foo).build();
     dbg!(&x);
-
-    assert_eq!(
-        x,
-        Foo {
-            field_a: 5,
-            field_b: 0,
-            field_c: 42,
-            field_d: String::from(""),
-            field_e: String::from("hello"),
-            field_f: String::from("world"),
-            field_g: vec![0, 1],
-            field_h: vec![2, 3],
-            field_i: vec![4, 5, 6],
-            field_j: String::from("hi"),
-        }
-    );
+    assert_eq!(x, expected!(Foo));
 }
--- tests/simple.rs
+++ tests/simple.rs
@@ -1,86 +1,69 @@
 #![allow(dead_code)]
 
+// enusre that the same functionality is generated for all kinds (simple)
+
 use bauer::Builder;
+
+macro_rules! define {
+    ($name: ident, $kind: literal) => {
+        #[derive(Debug, Builder, PartialEq)]
+        #[builder(kind = $kind, prefix = "set_")]
+        struct $name {
+            /// Hello
+            #[builder(default = "42")]
+            field_a: u32,
+            field_b: bool,
+            #[builder(into)]
+            field_c: String,
+            #[builder(skip_prefix, skip_suffix, rename = "add_d", repeat, repeat_n = 3..)]
+            field_d: Vec<f64>,
+        }
+    };
+}
+
+macro_rules! populate {
+    ($name: ident) => {
+        $name::builder()
+            .set_field_a(69)
+            .set_field_b(true)
+            .set_field_c("hello world")
+            .add_d(std::f64::consts::PI)
+            .add_d(std::f64::consts::TAU)
+            .add_d(2.72)
+    };
+}
+
+macro_rules! expected {
+    ($name: ident) => {
+        $name {
+            field_a: 69,
+            field_b: true,
+            field_c: "hello world".into(),
+            field_d: vec![std::f64::consts::PI, std::f64::consts::TAU, 2.72],
+        }
+    };
+}
 
 #[test]
 fn simple_owned() {
-    #[derive(Debug, Builder)]
-    #[builder(kind = "owned", prefix = "set_")]
-    struct Foo {
-        /// Hello
-        #[builder(default = "42")]
-        field_a: u32,
-        field_b: bool,
-        #[builder(into)]
-        field_c: String,
-        #[builder(skip_prefix, skip_suffix, rename = "add_d", repeat, repeat_n = 3..)]
-        field_d: Vec<f64>,
-    }
-
-    let x: Foo = Foo::builder()
-        .set_field_a(69)
-        .set_field_b(true)
-        .set_field_c("hello world")
-        .add_d(std::f64::consts::PI)
-        .add_d(std::f64::consts::TAU)
-        .add_d(2.72)
-        .build()
-        .unwrap();
-
-    dbg!(x);
+    define!(Foo, "owned");
+    let x = populate!(Foo).build().unwrap();
+    dbg!(&x);
+    assert_eq!(x, expected!(Foo));
 }
 
 #[test]
 fn simple_borrowed() {
-    #[derive(Debug, Builder)]
-    #[builder(kind = "borrowed", prefix = "set_")]
-    struct Foo {
-        /// Hello
-        #[builder(default = "42")]
-        field_a: u32,
-        field_b: bool,
-        #[builder(into)]
-        field_c: String,
-        #[builder(skip_prefix, skip_suffix, rename = "add_d", repeat, repeat_n = 3..)]
-        field_d: Vec<f64>,
-    }
-
-    let x: Foo = Foo::builder()
-        .set_field_a(69)
-        .set_field_b(true)
-        .set_field_c("hello world")
-        .add_d(std::f64::consts::PI)
-        .add_d(std::f64::consts::TAU)
-        .add_d(2.72)
-        .build()
-        .unwrap();
-
-    dbg!(x);
+    define!(Foo, "borrowed");
+    let x = populate!(Foo).build().unwrap();
+    dbg!(&x);
+    assert_eq!(x, expected!(Foo));
 }
 
 #[test]
 fn simple_type_state() {
-    #[derive(Debug, Builder)]
-    #[builder(kind = "type-state", prefix = "set_")]
-    struct Foo {
-        /// Hello
-        #[builder(default = "42")]
-        field_a: u32,
-        field_b: bool,
-        #[builder(into)]
-        field_c: String,
-        #[builder(skip_prefix, skip_suffix, rename = "add_d", repeat, repeat_n = 3..)]
-        field_d: Vec<f64>,
-    }
-
-    let x: Foo = Foo::builder()
-        .set_field_a(69)
-        .set_field_b(true)
-        .set_field_c("hello world")
-        .add_d(std::f64::consts::PI)
-        .add_d(std::f64::consts::TAU)
-        .add_d(2.72)
-        .build();
-
-    dbg!(x);
+    define!(Foo, "type-state");
+    let x = populate!(Foo).build();
+    dbg!(&x);
+    assert_eq!(x, expected!(Foo));
 }
