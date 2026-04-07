fc9ee83d73c1f548dfc6d48aaff4e609c5f278cf
funnyboy-roks <fbr@fbr.dev>
2026-04-07 04:31:30 -0500

    restructre and improve raw-identifiers

--- bauer/tests/raw-identifiers.rs
+++ bauer/tests/raw-identifiers.rs
@@ -6,17 +6,17 @@ macro_rules! tests {
     ($kind: literal in mod $module: ident $($unwrap: ident)?) => {
         mod $module {
 
-            use bauer::Builder;
-
-            #[derive(Builder)]
-            #[builder(kind = $kind)]
-            struct TestWithKeywords {
-                r#type: String,
-                r#async: Option<bool>,
-            }
-
             #[test]
-            fn test_raw_identifiers() {
+            fn fields() {
+                use bauer::Builder;
+
+                #[derive(Builder)]
+                #[builder(kind = $kind)]
+                struct TestWithKeywords {
+                    r#type: String,
+                    r#async: Option<bool>,
+                }
+
                 let instance = TestWithKeywords::builder()
                     .r#type("test".to_string())
                     .r#async(true)
@@ -25,6 +25,101 @@ macro_rules! tests {
 
                 assert_eq!(instance.r#type, "test");
                 assert_eq!(instance.r#async, Some(true));
+            }
+
+            #[test]
+            fn generics() {
+                #![allow(non_camel_case_types)]
+
+                use bauer::Builder;
+
+                #[derive(Builder)]
+                #[builder(kind = $kind)]
+                struct TestWithKeywords<r#type> {
+                    field: r#type,
+                }
+
+                let instance = TestWithKeywords::builder()
+                    .field("test".to_string())
+                    .build()
+                    $(.$unwrap())?;
+
+                assert_eq!(instance.field, "test");
+            }
+
+            #[test]
+            fn struct_name() {
+                #![allow(non_camel_case_types)]
+
+                use bauer::Builder;
+
+                #[derive(Builder)]
+                #[builder(kind = $kind)]
+                struct r#type {
+                    field: String,
+                }
+
+                let instance = r#type::builder()
+                    .field("test".to_string())
+                    .build()
+                    $(.$unwrap())?;
+
+                assert_eq!(instance.field, "test");
+            }
+
+            #[test]
+            fn rename() {
+                use bauer::Builder;
+
+                #[derive(Builder)]
+                #[builder(kind = $kind)]
+                struct Foo {
+                    #[builder(rename = "r#type")]
+                    field: String,
+                }
+
+                let instance = Foo::builder()
+                    .r#type("test".to_string())
+                    .build()
+                    $(.$unwrap())?;
+
+                assert_eq!(instance.field, "test");
+            }
+
+            #[test]
+            fn prefix() {
+                use bauer::Builder;
+
+                #[derive(Builder)]
+                #[builder(kind = $kind, prefix = "ty")]
+                struct Foo {
+                    pe: String,
+                }
+
+                let instance = Foo::builder()
+                    .r#type("test".to_string())
+                    .build()
+                    $(.$unwrap())?;
+
+                assert_eq!(instance.pe, "test");
+            }
+
+            #[test]
+            fn suffix() {
+                use bauer::Builder;
+
+                #[derive(Builder)]
+                #[builder(kind = $kind, suffix = "pe")]
+                struct Foo {
+                    ty: String,
+                }
+
+                let instance = Foo::builder()
+                    .r#type("test".to_string())
+                    .build()
+                    $(.$unwrap())?;
+
+                assert_eq!(instance.ty, "test");
             }
         }
     };
