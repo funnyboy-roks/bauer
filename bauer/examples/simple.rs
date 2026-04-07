169992ed54d343cd431a390f0f3eb15c7bbad974
funnyboy-roks <fbr@fbr.dev>
2026-04-06 20:07:10 -0500

    reset simple

--- bauer/examples/simple.rs
+++ bauer/examples/simple.rs
@@ -1,34 +1,27 @@
 use bauer::Builder;
 
 #[derive(Debug, Builder)]
-#[builder(kind = "type-state", prefix = "set_")]
+#[builder(kind = "owned", prefix = "set_")]
 pub struct Foo {
     #[builder(default = "42")]
     pub field_a: u32,
     pub field_b: bool,
-    // #[builder(into)]
-    // pub field_c: String,
-    #[builder(skip_prefix, skip_suffix, rename = "add_d", repeat = u8, repeat_n = 3, collector = field_d_collector)]
-    pub field_d: String,
-}
-
-fn field_d_collector(iter: impl Iterator<Item = u8>) -> String {
-    // [iter[0] + 2, iter[1] + 2, iter[2] + 2]
-    iter.map(char::from).collect()
+    #[builder(into)]
+    pub field_c: String,
+    #[builder(skip_prefix, skip_suffix, rename = "add_d", repeat)]
+    pub field_d: Vec<f64>,
 }
 
 fn main() {
     let x: Foo = Foo::builder()
         .set_field_a(69)
         .set_field_b(true)
-        // .set_field_c("hello world")
-        // .add_d(std::f64::consts::PI.to_string())
-        // .add_d(std::f64::consts::TAU.to_string())
-        // .add_d(2.72.to_string())
-        .add_d(3)
-        .add_d(4)
-        .add_d(5)
-        .build();
+        .set_field_c("hello world")
+        .add_d(std::f64::consts::PI)
+        .add_d(std::f64::consts::TAU)
+        .add_d(2.72)
+        .build()
+        .unwrap();
 
     dbg!(x);
 }
