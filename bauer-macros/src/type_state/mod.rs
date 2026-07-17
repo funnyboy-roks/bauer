fc966e65309a94af2064683933c88347b293049b
funnyboy-roks <fbr@fbr.dev>
2026-07-17 16:53:28 -0500

    unused_braces is not clippy

--- bauer-macros/src/type_state/mod.rs
+++ bauer-macros/src/type_state/mod.rs
@@ -187,7 +187,7 @@ fn build_fn(
 
     if let Some((param, ty, body)) = &builder_attr.build_fn.mapper {
         ret_val = quote! {
-            #[allow(clippy::unused_braces)]
+            #[allow(unused_braces)]
             #konst fn __private_mapper(#param: #ret_ty) -> #ty {
                 #body
             }
